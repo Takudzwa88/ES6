2acb6474c0737423a8f56c21febb85efdda03249
Takudzwa Kanokanga <kanokangatakudzwa@gmail.com>
2025-08-05 15:56:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+window.onload = function() {
+/*
+    const pi = 3.142;
+
+    function calcArea(r){
+        const pi = 10;
+        console.log("The area is: " + (pi * r * r));
+    }  
+    
+    console.log(pi);
+    calcArea(5);
+    */
+
+  /* 
+   var x = 10;
+
+    if(x > 5) {
+        let x = 5; // This will change the value of x in the global scope
+        console.log("inside: " + x);
+    }   
+
+    console.log("outside: " + x); 
+    */
+   /*
+    var items = document.getElementsByTagName("li");
+    for(let x = 0; x < items.length; x++) {
+     items[x].onclick = function() {
+         console.log(x);
+     }   
+    } */
+
+    /* function log(num=10) {
+        console.log(num);
+     }
+
+     log(20); */
+
+     /*function logNinja(name="Ryu", belt="Red", age=25){
+     console.log("My name is " + name + 
+        " and my belt colour is " + belt +
+        " and my age is " + age);
+     }
+
+     logNinja("Shaun", "pink", 40);*/
+
+     /*var meats = ["ham", "salami", "bacon"];
+
+     console.log(...meats);*/
+
+     /*var nums1 = [1,2,3];
+     var nums2 = [...nums1,4,5,6];
+
+     console.log(nums2);
+     */
+
+     /*var nums = [3,5,7]
+
+     function addNums(a,b,c){
+            console.log(a + b + c);
+        }
+
+        addNums(...nums); */
+
+        var temp = `hello, my name is ryu`;
+
+        console.log(temp);
+        
+}
